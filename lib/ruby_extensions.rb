959da8dd06651c76fee41055d6c7ab9c13604ab0
Paweł Gościcki <pawel.goscicki@gmail.com>
2015-12-13 22:56:22 +0100

    Expect everything to be UTF-8 (Ruby 1.9+ required change)
    In Ruby 1.9+ we must convert everything coming our way to UTF-8
    explicitly, as Rails will sometimes send it our way as binary
    (ASCII-8BIT).
    
    In Rails 3+ this should no longer be necessary.

--- lib/ruby_extensions.rb
+++ lib/ruby_extensions.rb
@@ -15,17 +15,20 @@ end
 String.class_eval do
 
   def to_slug
-    self.transliterate.downcase.gsub(/[^a-z0-9 ]/, ' ').strip.gsub(/[ ]+/, '-')
+    # TODO force_encoding could/should be removed when upgraded to Rails 3+
+    self.force_encoding('UTF-8').transliterate.downcase.gsub(/[^a-z0-9 ]/, ' ').strip.gsub(/[ ]+/, '-')
   end
 
   # differs from the 'to_slug' method in that it leaves in the dot '.' character and removes Windows' crust from paths (removes "C:\Temp\" from "C:\Temp\mieczyslaw.jpg")
   def sanitize_as_filename
-    self.gsub(/^.*(\\|\/)/, '').transliterate.downcase.gsub(/[^a-z0-9\. ]/, ' ').strip.gsub(/[ ]+/, '-')
+    # TODO force_encoding could/should be removed when upgraded to Rails 3+
+    self.force_encoding('UTF-8').gsub(/^.*(\\|\/)/, '').transliterate.downcase.gsub(/[^a-z0-9\. ]/, ' ').strip.gsub(/[ ]+/, '-')
   end
 
   def transliterate
     # Unidecoder gem is missing some hyphen transliterations
-    self.gsub(/[-‐‒–—―⁃−­]/, '-').to_ascii
+    # TODO force_encoding could/should be removed when upgraded to Rails 3+
+    self.force_encoding('UTF-8').gsub(/[-‐‒–—―⁃−­]/, '-').to_ascii
   end
 
 end
